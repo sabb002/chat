0bc484c31344ad895becdf831ff0e3c166633d24
sabb002 <118009228+sabb002@users.noreply.github.com>
2023-06-01 02:26:13 +0600

    bug fix

--- src/Components/TextMessage.tsx
+++ src/Components/TextMessage.tsx
@@ -8,18 +8,17 @@ interface Props {
 
 export default function TextMessage({ userName, currentUser, text }: Props) {
   const isCurrentUser = userName === currentUser;
-  console.log(isCurrentUser);
 
   const formattedText = text.replace(/\n/g, "<br/>");
   return (
     <div
       className={`mx-3 py-3 flex items-end ${
-        isCurrentUser ? " flex-row-reverse text-right" : "text-left"
+        isCurrentUser ? " flex-row-reverse " : ""
       }`}
     >
       <div
         className={`w-[25px] h-[25px] flex flex-shrink-0 items-center justify-center m-2 ${
-          isCurrentUser ? "bg-blue-600 text-white" : "bg-gray-200"
+          isCurrentUser ? " bg-blue-600 text-white" : "bg-gray-200"
         } rounded-full`}
       >
         <BsFillPersonFill size={15} />
@@ -33,8 +32,12 @@ export default function TextMessage({ userName, currentUser, text }: Props) {
         <div className=" text-xs text-white">{userName}</div>
 
         <div
-          className={` h-fit px-3 py-1 bg-gray-200 text-xs md:text-sm text-black font-semibold rounded-tr-lg rounded-br-lg rounded-bl-lg whitespace-pre-wrap
-          ${isCurrentUser ? " bg-blue-500 text-white" : ""}`}
+          className={` h-fit px-3 py-1  text-xs md:text-sm text-black font-semibold  rounded-br-lg rounded-bl-lg whitespace-pre-wrap
+          ${
+            isCurrentUser
+              ? "bg-blue-500 text-white rounded-tl-lg"
+              : "bg-gray-200 rounded-tr-lg"
+          }`}
           dangerouslySetInnerHTML={{ __html: formattedText }}
         ></div>
       </div>
